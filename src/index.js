9d78f939040e807b5af251d99f5427114ce20dd9
Manmeet <manmeetsgujral@gmail.com>
2016-07-05 22:37:01 -0400

    Finished assignment in terms of code and installation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
+import './style.scss';
 
 let num = 0;
-setInterval(function() {
+setInterval(() => {
   $('#main').html(`You've been on this page for ${num++} seconds.`);
 }, 1000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
+  stats: { colors: true },
+  devtool: 'inline-source-map',
   entry: ['babel-polyfill', './src'],
   output: {
     path: 'build',
     publicPath: 'build/',
     filename: 'bundle.js',
-    // outputs everything to build/bundle.js
   },
   module: {
     loaders: [{
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'babel',
-  }],
-},
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    },
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+    },
+    {
+      test: /\.scss/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+    },
+      // You could also use other loaders the same way. I. e. the autoprefixer-loader
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
+
 };
