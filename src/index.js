882aa2cbd08a422921c0debfbae67588bc7ba106
Manmeet <manmeetsgujral@gmail.com>
2016-07-07 13:33:28 -0400

    Added changing colors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import $ from 'jquery';
 import './style.scss';
 
-let num = 0;
+let num = 1;
 setInterval(() => {
   $('#main').html(`You've been on this page for ${num++} seconds.`);
 }, 1000);
+
+const colors = ['bisque', 'darkseagreen', 'white', 'lightsteelblue'];
+let index = 0;
+setInterval(() => {
+  $('body').css({ background: colors[index++ % 4] });
+}, 10000);
